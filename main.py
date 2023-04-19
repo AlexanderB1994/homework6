8b1998f0247a243d85afd33d6871c72eb12f2fef
AlexanderB1994 <bulgaru.alexander1994@gmail.com>
2023-04-19 13:56:54 +0300

    v1

---
+++ main.py
@@ -0,0 +1,41 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def __add__(self, other):
+        new_numerator = (self.numerator * other.denominator) + (other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __sub__(self, other):
+        new_numerator = (self.numerator * other.denominator) - (other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def inverse(self):
+        return Fraction(self.denominator, self.numerator)
+
+
+f1 = Fraction(2, 4)
+f2 = Fraction(3, 6)
+
+if __name__ == '__main__':
+    print(f1)
+    print(f2)
+
+    # add
+    f3 = f1 + f2
+    print(f3)
+
+    # sub
+    f4 = f1 - f2
+    print(f4)
+
+    # inverse
+    f5 = Fraction(3, 4)
+    f6 = f5.inverse()
+    print(f6)
