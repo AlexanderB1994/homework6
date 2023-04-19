7f9a7754004c0bd426b67e63285c665c05eb17bb
AlexanderB1994 <125302110+AlexanderB1994@users.noreply.github.com>
2023-04-19 14:45:16 +0300

    Add files via upload
    v2

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ class Fraction:
     def __str__(self):
         return f"{self.numerator}/{self.denominator}"
 
-    def __add__(self, other):
-        new_numerator = (self.numerator * other.denominator) + (other.numerator * self.denominator)
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+    def combine(self, other, op):
+        if op == "+":
+            new_numerator = (self.numerator * other.denominator) + (other.numerator * self.denominator)
+        elif op == "-":
+            new_numerator = (self.numerator * other.denominator) - (other.numerator * self.denominator)
+        else:
+            raise ValueError("Unsupported operation")
 
-    def __sub__(self, other):
-        new_numerator = (self.numerator * other.denominator) - (other.numerator * self.denominator)
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
 
@@ -24,16 +25,16 @@ f1 = Fraction(2, 4)
 f2 = Fraction(3, 6)
 
 if __name__ == '__main__':
-    #str
+    # str
     print(f1.numerator, f1.denominator)
     print(f2.numerator, f2.denominator)
 
     # add
-    f3 = f1 + f2
+    f3 = f1.combine(f2, "+")
     print(f3)
 
     # sub
-    f4 = f1 - f2
+    f4 = f1.combine(f2, "-")
     print(f4)
 
     # inverse
