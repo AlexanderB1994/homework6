6cd709593d1eb8a19b8e8b8e513d10a5e2b53e10
AlexanderB1994 <125302110+AlexanderB1994@users.noreply.github.com>
2023-04-19 14:47:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ class Fraction:
     def __str__(self):
         return f"{self.numerator}/{self.denominator}"
 
-    def combine(self, other, op):
-        if op == "+":
-            new_numerator = (self.numerator * other.denominator) + (other.numerator * self.denominator)
-        elif op == "-":
-            new_numerator = (self.numerator * other.denominator) - (other.numerator * self.denominator)
-        else:
-            raise ValueError("Unsupported operation")
+    def __add__(self, other):
+        new_numerator = (self.numerator * other.denominator) + (other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
 
+    def __sub__(self, other):
+        new_numerator = (self.numerator * other.denominator) - (other.numerator * self.denominator)
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
 
@@ -30,11 +29,11 @@ if __name__ == '__main__':
     print(f2.numerator, f2.denominator)
 
     # add
-    f3 = f1.combine(f2, "+")
+    f3 = f1 + f2
     print(f3)
 
     # sub
-    f4 = f1.combine(f2, "-")
+    f4 = f1 - f2
     print(f4)
 
     # inverse
